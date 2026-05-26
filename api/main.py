372a5fa50906f2f2a1471396de836dc173b40c00
anaclaraac <anaclaracostagoncalves145@gmail.com>
2026-05-26 18:15:04 -0300

    integrar modelo na api

--- api/main.py
+++ api/main.py
@@ -1,18 +1,33 @@
 from fastapi import FastAPI
 
+import pandas as pd
+import joblib
+
+
 app = FastAPI()
 
+modelo = joblib.load(
+    "ml/model.pkl"
+)
 
-@app.get("/")
 
-def home():
+@app.get("/prever")
+
+def prever():
+
+    entrada = pd.DataFrame({
+        "home_attack_avg":[2.0],
+        "away_attack_avg":[1.5],
+        "home_form":[0.8],
+        "attack_strength":[2.1],
+        "avg_goals":[2.5]
+    })
+
+    resultado = modelo.predict(
+        entrada
+    )
 
     return {
-
-        "projeto":
-        "CopaPredict AI",
-
-        "status":
-        "ativo"
-
+        "previsao":
+        int(resultado[0])
     }
